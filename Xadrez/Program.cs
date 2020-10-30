54e51f15c9e302a449c8549e307373d8d6e851bc
Eduardo Berg <duhberg@gmail.com>
2020-10-29 22:58:10 -0300

    Tabuleiro e algumas peças feitas

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using tabuleiro;
+using xadrez;
 
 namespace Xadrez
 {
@@ -6,7 +8,13 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0,0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1,3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2,4));
+
+            Tela.imprimirTabuleiro(tab);
         }
     }
 }
---
+++ Xadrez/Tela.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using tabuleiro;
+
+namespace Xadrez
+{
+    class Tela
+    {
+        public static void imprimirTabuleiro(Tabuleiro tab)
+        {
+
+            for (int i = 0; i < tab.Linhas; i++)
+            {
+                for (int x = 0; x < tab.Colunas; x++)
+                {
+                    if (tab.peca(i, x) == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        Console.Write(tab.peca(i, x) + " ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+
+
+}
+
